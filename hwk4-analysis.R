5fa1bbb50a0603a23c541b1c614d30b1faaf6e5a
benysh75 <benysh75@gmail.com>
2023-04-05 09:36:07 -0400

    version2

--- hwk4-analysis.R
+++ hwk4-analysis.R
@@ -20,15 +20,12 @@ ma.data <- read_rds("data/output/final_ma_data.rds")
 
 ## Question 1 Distribution of plan counts by county over time ------------------
 
-ma.data.selected <- ma.data %>%
-  filter(snp == "No" & (planid < 800 | planid >= 900) & partd == "No")
-
-q1.data <- ma.data.selected %>%
+q1.data <- ma.data %>%
   group_by(year, county) %>%
   summarise(count = length(planid))
 
 q1.data.sum <- q1.data %>%
-  group_by(year) %>% 
+  group_by(factor(year)) %>% 
   summarise(`Mean` = mean(count),
             `Max` = max(count),
             `Q2` = quantile(count, 0.25),
@@ -37,10 +34,11 @@ q1.data.sum <- q1.data %>%
             `Min` = min(count))
 
 q1.plot <- q1.data %>%
-  ggplot(aes(x = factor(year), y = log(count))) +
-  geom_boxplot(size = 0.5, color = "dodgerblue4", fill = "dodgerblue1", alpha = 0.25, outlier.color = "dodgerblue4", outlier.shape = 16) +
-  # stat_summary(fun.y = "mean", geom = "point", shape = 16, size = 4, color = "dodgerblue") +
+  ggplot(aes(x = factor(year), y = count)) +
+  geom_boxplot(size = 0.5, color = "dodgerblue4", fill = "dodgerblue1", alpha = 0.25, outlier.color = "dodgerblue4", outlier.shape = NA) +
+  stat_summary(fun.y = "mean", geom = "point", shape = 16, size = 4, color = "dodgerblue") +
   labs(x = "Year", y = "Log Plan Counts by County", Title = "Distribution of Plan Counts by County from 2007 to 2015") +
+  ylim(0,100) +
   theme_bw() + theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank()) +
   theme(
     plot.title = element_text(size = 12, color = "black", hjust = 0.5),
@@ -140,17 +138,20 @@ q3.plot <- q3.data %>%
 
 q4.data <- ma.data %>%
   filter(year %in% 2009:2015) %>%
+  group_by(fips, year) %>%
+  summarise(all_enroll = first(avg_enrolled),
+            all_eligible = first(avg_eligibles),
+            bench = mean(ma_rate, na.rm = TRUE),
+            share = all_enroll/all_eligible) %>%
   group_by(year) %>%
-  summarise(all_enroll = sum(avg_enrollment, na.rm = TRUE),
-            all_eligible = sum(avg_eligibles, na.rm = TRUE),
-            share = all_enroll/all_eligible * 100)
+  summarise(avg_share = mean(share, na.rm = TRUE))
 
 q4.plot <- q4.data %>%
-  ggplot(aes(x = factor(year), y = share)) +
-  geom_col(width = 0.5, fill = "dodgerblue4") +
-  geom_text(label = paste0(round(q4.data$share,2), "%"), size = 3, nudge_x = 0, nudge_y = 0.025, check_overlap = TRUE) +
-  ylim(0,1) +
-  labs(x = "Year", y = "Share (%)", Title = "Average Share of Medicare Advantage (Relative to All Medicare Eligibles) from 2009 to 2015") +
+  ggplot(aes(x = year, y = avg_share)) +
+  geom_line(size = 1, color = "dodgerblue4") +
+  scale_x_continuous(breaks = c(2009:2015)) +
+  geom_text(label = round(q4.data$avg_share,2), size = 3, nudge_x = 0, nudge_y = 0.0025, check_overlap = TRUE) +
+  labs(x = "Year", y = "Average Share", Title = "Average Share of Medicare Advantage (Relative to All Medicare Eligibles) from 2009 to 2015") +
   theme_bw() + theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank()) +
   theme(
     plot.title = element_text(size = 12, color = "black", hjust = 0.5),
@@ -189,24 +190,20 @@ colnames(q5.data) <- c("Star Ratings", "Count")
 
 bandwidth <- c(0.125, 0.1, 0.12, 0.13, 0.14, 0.15)
 starRating <- c(3, 3.5, 4)
-ma.rd.ls <- list()
-for (j in length(bandwidth)){
-  for (i in length(starRating)){
-    ma.rd <- ma.data.clean %>% filter(Star_Rating == starRating[i]-0.5 | Star_Rating == starRating[i])
+for (j in 1:length(bandwidth)){
+  for (i in 1:length(starRating)){
+    ma.rd <- ma.data.clean %>%
+      filter(Star_Rating == starRating[i]-0.5 | Star_Rating == starRating[i]) %>%
+      mutate(ratingScore = raw_rating - (starRating[i]-0.25))
+    
     ma.rd.dist <- ma.rd %>% ggplot(aes(x = raw_rating)) + geom_bar(width = .025) + theme_bw() +
-      labs(x = "Running Variable", y = "Number of Plans", title = "Distribution of Raw Scores") 
+      labs(x = "Running Variable", y = "Number of Plans", title = "Distribution of Raw Ratings") 
     
-    ma.rd <-ma.rd %>%
-      mutate(score = raw_rating - (starRating[i]-0.25),
-             treat = (score >= 0),
-             window1 = (score >= -.175 & score <= .175),
-             window2 = (score >= -.125 & score <= .125),
-             enrollment = avg_enrollment,
-             score_treat = score*treat)
-    
-    ma.rd.plot <- rdplot(y = ma.rd$enrollment, x = ma.rd$score, binselect = "es",
-                         title = "RD Plot: Enrollment", x.label = "Summary Score", y.label = "Enrollment", masspoints = "off")
-    ma.rd.est <- rdrobust(y = ma.rd$enrollment, x = ma.rd$score, c = 0, h = bandwidth[j], p = 1,
+    ma.rd.plot <- rdplot(y = ma.rd$avg_enrollment, x = ma.rd$ratingScore,
+                         binselect = "es",
+                         title = "RD Plot: Enrollment", x.label = "Star Rating", y.label = "Enrollment", masspoints = "off")
+    ma.rd.est <- rdrobust(y = ma.rd$avg_enrollment, x = ma.rd$ratingScore,
+                          c = 0, h = bandwidth[j], p = 1,
                           kernel = "uniform", vce = "hc0", masspoints = "off")
     
     ma.rd.bin.avg <- as_tibble(ma.rd.plot$vars_bins)
@@ -216,24 +213,25 @@ for (j in length(bandwidth)){
       scale_x_continuous(breaks = c(.5, 1.5), label = c("Untreated", "Treated")) +
       xlab("Running Variable") + ylab("Outcome")
     
-    ma.rd.ls[[j]][[i]][1] <- ma.rd
-    ma.rd.ls[[j]][[i]][2] <- ma.rd.dist
-    ma.rd.ls[[j]][[i]][3] <- ma.rd.plot
-    ma.rd.ls[[j]][[i]][4] <- ma.rd.est
-    ma.rd.ls[[j]][[i]][5] <- ma.rd.bin.avg
-    ma.rd.ls[[j]][[i]][6] <- ma.rd.plot.bin
+    ma.rd_dens <- rddensity(ma.rd$ratingScore, c = 0)
+    ma.rd_dens_plot <- (rdplotdensity(ma.rd_dens, ma.rd$ratingScore))$Estplot
+    
+    assign(paste0("ma.rd.est_", bandwidth[j], "_", starRating[i]), ma.rd.est)
+    assign(paste0("ma.rd_dens_plot_", bandwidth[j], "_", starRating[i]), ma.rd_dens_plot)
   }
 }
 
-ma.rd.ls[[6]]
-
-q6.data <- cbind(ma.rd.est_3$coef, ma.rd.est_3.5$coef, ma.rd.est_4$coef)
+q6.data <- cbind(ma.rd.est_0.125_3$coef, ma.rd.est_0.125_3.5$coef, ma.rd.est_0.125_4$coef)
 colnames(q6.data) <- c("3-Star", "3.5-Star", "4-Star")
 
-## Question 8 Manipulate the Running Variable ----------------------------------
+q7.data <- cbind(ma.rd.est_0.1_3$coef, ma.rd.est_0.1_3.5$coef, ma.rd.est_0.1_4$coef,
+                 ma.rd.est_0.12_3$coef, ma.rd.est_0.12_3.5$coef, ma.rd.est_0.12_4$coef,
+                 ma.rd.est_0.13_3$coef, ma.rd.est_0.13_3.5$coef, ma.rd.est_0.13_4$coef,
+                 ma.rd.est_0.14_3$coef, ma.rd.est_0.14_3.5$coef, ma.rd.est_0.14_4$coef,
+                 ma.rd.est_0.15_3$coef, ma.rd.est_0.15_3.5$coef, ma.rd.est_0.15_4$coef)
+colnames(q7.data) <- rep(c("3-Star", "3.5-Star", "4-Star"), 5)
 
-ma.rd_3_dens <- rddensity(ma.rd_3$score, c=0)
-q8.plot <- rdplotdensity(ma.rd_3_dens, ma.rd_3$score)
+## Question 8 Manipulate the Running Variable ----------------------------------
 
 ## Question 9 HMO and Part D status as Plan Characteristics --------------------
 
@@ -244,7 +242,6 @@ m <- lm(formula = avg_enrollment ~ factor(Star_Rating) + factor(partd) + factor(
 colnames(ma.data)
 rm(list=c("ma.data"))
 save.image("Hwk4_workspace.Rdata")
-levels(factor(ma.data.clean$plan_type))
 
 # %>%
 # select(contractid, planid, fips, avg_enrollment, first_enrolled, last_enrolled, 
